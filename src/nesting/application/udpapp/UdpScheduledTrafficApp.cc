f3eecf3977867089eef590500a1dad6bc0945d97
Adriaan Nieß <mail@adriaan-niess.com>
2019-09-16 12:01:54 +0200

    Add UDP app skeleton for generating scheduled traffic

---
+++ src/nesting/application/udpapp/UdpScheduledTrafficApp.cc
@@ -0,0 +1,67 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#include "UdpScheduledTrafficApp.h"
+
+namespace nesting {
+
+Define_Module(UdpScheduledTrafficApp);
+
+void UdpScheduledTrafficApp::initialize()
+{
+    // TODO - Generated method body
+}
+
+void UdpScheduledTrafficApp::handleMessage(cMessage *msg)
+{
+    // TODO - Generated method body
+}
+
+void UdpScheduledTrafficApp::handleMessageWhenUp(cMessage *msg)
+{
+    // TODO
+}
+
+void UdpScheduledTrafficApp::handleStartOperation(inet::LifecycleOperation *operation)
+{
+    // TODO
+}
+
+void UdpScheduledTrafficApp::handleStopOperation(inet::LifecycleOperation *operation)
+{
+    // TODO
+}
+
+void UdpScheduledTrafficApp::handleCrashOperation(inet::LifecycleOperation *operation)
+{
+    // TODO
+}
+
+void UdpScheduledTrafficApp::socketDataArrived(inet::UdpSocket *socket, inet::Packet *packet)
+{
+    // TODO
+}
+
+void UdpScheduledTrafficApp::socketErrorArrived(inet::UdpSocket *socket, inet::Indication *indication)
+{
+    // TODO
+}
+
+void UdpScheduledTrafficApp::socketClosed(inet::UdpSocket *socket)
+{
+    // TODO
+}
+
+} //namespace
