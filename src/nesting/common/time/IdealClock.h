e575de2f6bcee934a76ca6d9d91ce56ea6b2bb9a
Adriaan Nieß <mail@adriaan-niess.com>
2020-03-29 14:20:22 +0200

    Use consistent format for include guards

--- src/nesting/common/time/IdealClock.h
+++ src/nesting/common/time/IdealClock.h
@@ -13,8 +13,8 @@
 // along with this program.  If not, see http://www.gnu.org/licenses/.
 // 
 
-#ifndef __MAIN_IDEALCLOCK_H_
-#define __MAIN_IDEALCLOCK_H_
+#ifndef NESTING_COMMON_TIME_IDEALCLOCK_H_
+#define NESTING_COMMON_TIME_IDEALCLOCK_H_
 
 #include <omnetpp.h>
 
--- src/nesting/common/time/OscillatorBase.h
+++ src/nesting/common/time/OscillatorBase.h
@@ -13,8 +13,8 @@
 // along with this program.  If not, see http://www.gnu.org/licenses/.
 // 
 
-#ifndef __NESTINGNG_OSCILLATORBASE_H_
-#define __NESTINGNG_OSCILLATORBASE_H_
+#ifndef NESTING_COMMON_TIME_OSCILLATORBASE_H_
+#define NESTING_COMMON_TIME_OSCILLATORBASE_H_
 
 #include <omnetpp.h>
 
